78405ffd41ab1b0a6d07fad38b205b4ee86c2e80
infozubia <infozubia@DESKTOP-2PGL44N>
2023-10-18 08:23:59 +0200

    Agregar archivos de proyecto.

---
+++ Models/TaskModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Tasky.Models
+{
+    public class TaskModel
+
+    { //hiru propietate
+
+        public string TaskName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    }
+}
